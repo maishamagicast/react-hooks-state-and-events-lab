be231a46ccfd421019b768bb35cba83fb4c79bcb
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-05-05 21:55:48 +0300

    passed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useState } from 'react';
+
 
 function Item({ name, category }) {
+  const [inCart, placeInCart]= useState(false);
+  function clickOn(){
+    placeInCart(!inCart);
+  }
+  const itemInfo = !inCart ? 'in-cart': '';
+  const itemStatus = !inCart ? 'Add to Cart' : 'Remove From Cart';
   return (
-    <li className="">
+    <li className={itemInfo}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={clickOn}>{itemStatus}</button>
     </li>
   );
 }
